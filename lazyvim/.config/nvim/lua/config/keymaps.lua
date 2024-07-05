36f834b611cd8077c5df2f07349a68fa09d4f3d2
Ramon Voges <ramon.voges@gmail.com>
2024-07-05 20:16:26 +0200

    Lazyvim voll integrieren

---
+++ lazyvim/.config/nvim/lua/config/keymaps.lua
@@ -0,0 +1,8 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+
+-- vim.keymap.set("n", "<Leader>db", "<cmd> DapToggleBreakpoint <cr>")
+-- vim.keymap.set("n", "<Leader>dpr", function()
+--   require("dap-python").test_method()
+-- end)
